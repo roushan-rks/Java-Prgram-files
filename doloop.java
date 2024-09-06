c994b06e4d4ae458dc22caaaa331f740817876c3
ritik <rajjritik48@gmail.com>
2024-09-06 22:59:17 +0530

    loops

---
+++ doloop.java
@@ -0,0 +1,14 @@
+class doloop
+{
+    public static void main(String[] args) 
+    {
+    
+        int i = 5;
+        do//The do-while loop will execute the body of the loop at least once even the loop is false
+    {
+            System.out.println("Hi");
+        i++;
+    }
+    while(i<=4);
+}
+}
