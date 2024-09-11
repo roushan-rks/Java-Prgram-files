fc9a2a6a531d18168336b39ef3e818ddb631f250
ritik <rajjritik48@gmail.com>
2024-09-11 23:48:16 +0530

    my program

---
+++ jar.java
@@ -0,0 +1,9 @@
+class jar
+{
+    public static void main (String args[])
+    {
+        int x = 28;
+      if (x>20 && x<=30);//here we are using if statement to see wether the statement is true or false
+      System.out.println("ritik");
+    }
+}
